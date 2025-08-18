1ab1b11d5c20e000e7f7056c34f10476c3fc244f
garanfubao <ducanh.readystation@gmail.com>
2025-08-18 14:09:14 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,8 @@ export default defineConfig({
   },
   server: { host: true, port: 5173 },
   preview: { port: 5173 },
-  build: { target: 'es2020' }
+    build: {
+    target: 'es2020',
+    outDir: 'www'
+  }
 })
